996705149d62bf72a2ea520cbb2caeed9c0ec02f
Lucas Pinheiro <lucaslogos@gmail.com>
2022-02-19 15:57:07 -0300

    update

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 @Component({
   selector:"app-nova-transferencia",
   templateUrl:'./nova-transferencia.component.html',
-  styleUrls:['./nova-transferencia.components.scss']
+  styleUrls:['./nova-transferencia.components.css']
 })
 export class NovaTransferenciaComponent{
 
