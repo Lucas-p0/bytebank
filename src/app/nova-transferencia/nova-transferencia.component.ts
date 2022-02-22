678d0753455c14a1284b3c7d1dec0d50c0a4e220
Lucas Pinheiro <lucaslogos@gmail.com>
2022-02-21 23:43:09 -0300

    update

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import * as EventEmitter from 'events';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.components.css'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  valor: number;
+  destino: number;
+
   transferir() {
     console.log('Soliciatada nova transferência');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
   }
 }
